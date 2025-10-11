5048189971829b4f14018c75a9ac9e93f8c45874
Killian Baillifard <killian.baillifard@gmail.com>
2025-10-11 18:46:50 +0200

    Adapted i2c interface function names

--- src/drv8214_platform.c
+++ src/drv8214_platform.c
@@ -10,24 +10,24 @@
 
 #include <cmsis_os2.h>
 #include "drv8214_platform.h"
-#include "i2c.h"
+#include "i2c_dma.h"
 
 // Definitions
 
 uint8_t drv8214_read(Drv8214 *driver, uint8_t reg) {
     uint8_t data;
-    i2c_lock(driver->hi2c);
-    pca9546a_select_single_channel(driver->mux, driver->muxChannel); // At this point, driver value has been corrupted and cause hard fault, while it was not on last stack frame
-    i2c_write(driver->hi2c, (uint16_t)(driver->address << 1), &reg, 1);
-    i2c_read(driver->hi2c, (uint16_t)(driver->address << 1), &data, 1);
-    i2c_unlock(driver->hi2c);
+    i2c_dma_lock(driver->hi2c);
+    pca9546a_select_single_channel(driver->mux, driver->muxChannel);
+    i2c_dma_write(driver->hi2c, (uint16_t)(driver->address << 1), &reg, 1);
+    i2c_dma_read(driver->hi2c, (uint16_t)(driver->address << 1), &data, 1);
+    i2c_dma_unlock(driver->hi2c);
     return data;
 }
 
 void drv8214_write(Drv8214 *driver, uint8_t reg, uint8_t value) {
     uint8_t data[2] = {reg, value};
-    i2c_lock(driver->hi2c);
+    i2c_dma_lock(driver->hi2c);
     pca9546a_select_single_channel(driver->mux, driver->muxChannel);
-    i2c_write(driver->hi2c, (uint16_t)(driver->address << 1), data, 2);
-    i2c_unlock(driver->hi2c);
+    i2c_dma_write(driver->hi2c, (uint16_t)(driver->address << 1), data, 2);
+    i2c_dma_unlock(driver->hi2c);
 }
